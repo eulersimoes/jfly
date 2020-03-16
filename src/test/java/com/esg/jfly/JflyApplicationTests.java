b2538d407827785cae40e2c2a40161b8d2017940
eulersimoes <eulersimoes@gmail.com>
2020-03-15 22:38:16 -0300

    Initial artifact´s generated from spring boot.

---
+++ src/test/java/com/esg/jfly/JflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.jfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
